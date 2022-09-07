e260f7d75cd9e3744c525061031d9dd253ddab6a
MSYogeswari <112486454+MSYogeswari@users.noreply.github.com>
2022-09-07 14:41:16 +0530

    Create 2_Ricebag_at64box.java

---
+++ 2_Ricebag_at64box.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+       BigInteger result =  new BigInteger("1");
+        for(int i=2;i<=64;i++)
+        {
+            result = result.add(result);
+        }
+        System.out.println("sum of rice bags at 64th box " + result);
+     }
+}
